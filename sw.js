b3e66ae9056e630f8d874c2bb9ae7324e31b21fd
Your Name <your_email@example.com>
2025-10-20 21:48:08 +0100

    Updated sw,js,manifest,json

--- sw.js
+++ sw.js
@@ -1,30 +1,36 @@
 const CACHE_NAME = 'temperature-converter-v1';
 
+// Use the install event to pre-cache all initial resources.
 self.addEventListener('install', event => {
-  event.waitUntil((async () => {
-    const cache = await caches.open(CACHE_NAME);
-    await cache.addAll([
-      './',
-      './index.html',
-      './converter.js',
-      './converter.css',
-      './manifest.json',
-      './icon512.png'
-    ]);
-  })());
+    event.waitUntil((async () => {
+        const cache = await caches.open(CACHE_NAME);
+        cache.addAll([
+            '/',
+            '/converter.js',
+            '/converter.css'
+        ]);
+    })());
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith((async () => {
-    const cache = await caches.open(CACHE_NAME);
-    const cachedResponse = await cache.match(event.request);
-    if (cachedResponse) return cachedResponse;
-    try {
-      const fetchResponse = await fetch(event.request);
-      cache.put(event.request, fetchResponse.clone());
-      return fetchResponse;
-    } catch (e) {
-      return new Response('Offline', { status: 503 });
-    }
-  })());
+    event.respondWith((async () => {
+        const cache = await caches.open(CACHE_NAME);
+
+        // Get the resource from the cache.
+        const cachedResponse = await cache.match(event.request);
+        if (cachedResponse) {
+            return cachedResponse;
+        } else {
+            try {
+                // If the resource was not in the cache, try the network.
+                const fetchResponse = await fetch(event.request);
+
+                // Save the resource in the cache and return it.
+                cache.put(event.request, fetchResponse.clone());
+                return fetchResponse;
+            } catch (e) {
+                // The network failed.
+            }
+        }
+    })());
 });
